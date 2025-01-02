3f2b08da6e73a2cbac940bde6ab44cf7c69c8c8b
Wataru Shimizu <waruzilla@gmail.com>
2025-01-03 02:42:24 +0900

    Navigate with right and left key

--- imview.py
+++ imview.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python
 
+import glob
+import os
 from PySide6 import QtCore, QtGui, QtWidgets
+
+class ImageDirectoryNavigator:
+    def __init__(self, img_dir, initial_fname:str|None=None):
+        self.img_dir = img_dir
+        self.img_paths = sorted(list(glob.glob(os.path.join(img_dir, "*.JPG"))))
+        if initial_fname:
+            self.current_img_idx = self.img_paths.index(os.path.join(img_dir, initial_fname))
+        else:
+            self.current_img_idx = 0
+
+    def get_current_img_path(self):
+        return self.img_paths[self.current_img_idx]
+
+    def get_next_img_path(self):
+        self.current_img_idx = (self.current_img_idx + 1) % len(self.img_paths)
+        return self.get_current_img_path()
+
+    def get_prev_img_path(self):
+        self.current_img_idx = (self.current_img_idx - 1) % len(self.img_paths)
+        return self.get_current_img_path()
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
+        self.image_directory_navigator:ImageDirectoryNavigator|None = None
+
         super(MainWindow, self).__init__(parent)
         self.setWindowTitle("ImView")
 
@@ -26,17 +50,37 @@ class MainWindow(QtWidgets.QMainWindow):
         central_widget = QtWidgets.QWidget()
         central_widget.setLayout(vertical_layout)
         self.setCentralWidget(central_widget)
-        self.label = QtWidgets.QLabel("hello")
+        self.label = QtWidgets.QLabel("Open from File->Open or drag and drop an image file here")
         vertical_layout.addWidget(self.label)
 
         self.statusBar().showMessage("Ready")
+
+        self.open_new_file("../sample-images/EOS100D-2024-resized/IMG_5535.JPG")
+
         self.show()
 
+    def open_new_file(self, img_path):
+        img_dir, img_fname = os.path.split(img_path)
+        self.image_directory_navigator = ImageDirectoryNavigator(img_dir, img_fname)
+        self.load_and_show_image(self.image_directory_navigator.get_current_img_path())
+
     def open_file(self):
-        file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open Image File", QtCore.QDir.homePath())
-        if file_name:
-            self.pixmap = QtGui.QPixmap(file_name)
-            self.label.setPixmap(self.pixmap)
+        img_path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open Image File", QtCore.QDir.homePath())
+        if img_path:
+            self.load_and_show_image(img_path)
+
+    def load_and_show_image(self, img_path):
+        self.pixmap = QtGui.QPixmap(img_path)
+        self.label.setPixmap(self.pixmap)
+
+    def keyPressEvent(self, e):
+        if e.key() == QtCore.Qt.Key_Right:
+            self.image_directory_navigator.get_next_img_path()
+            self.load_and_show_image(self.image_directory_navigator.get_current_img_path())
+        elif e.key() == QtCore.Qt.Key_Left:
+            self.image_directory_navigator.get_prev_img_path()
+            self.load_and_show_image(self.image_directory_navigator.get_current_img_path())
+        return super().keyPressEvent(e)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication()
