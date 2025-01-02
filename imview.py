b6307e657befc267aae523656761c91446512253
Wataru Shimizu <waruzilla@gmail.com>
2025-01-03 01:36:27 +0900

    Add menu bar and remove ui file

--- imview.py
+++ imview.py
@@ -1,22 +1,44 @@
 #!/usr/bin/env python
 
-import sys
-from PySide6 import QtCore, QtWidgets, QtGui
-from PySide6.QtUiTools import QUiLoader
+from PySide6 import QtCore, QtGui, QtWidgets
 
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, parent=None) -> None:
+    def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setWindowTitle("ImView")
 
-        self.loader = QUiLoader()
-        self.window = self.loader.load("./mainwindow.ui", None)
-        self.setCentralWidget(self.window)
-        self.pixmap = QtGui.QPixmap("./sample1.jpg")
-        self.window.label.setPixmap(self.pixmap)
+        open_action = QtGui.QAction("&Open", self)
+        open_action.setShortcut("Ctrl+O")
+        open_action.setStatusTip("Open an image file")
+        open_action.triggered.connect(self.open_file)
 
-QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
-app = QtWidgets.QApplication(sys.argv)
-window = MainWindow()
-window.show()
-exit(app.exec())
+        exit_action = QtGui.QAction("&Exit", self)
+        exit_action.setShortcut("Ctrl+Q")
+        exit_action.setStatusTip("Exit the application")
+        exit_action.triggered.connect(self.close)
+
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu("&File")
+        file_menu.addAction(open_action)
+        file_menu.addAction(exit_action)
+
+        vertical_layout = QtWidgets.QVBoxLayout()
+        central_widget = QtWidgets.QWidget()
+        central_widget.setLayout(vertical_layout)
+        self.setCentralWidget(central_widget)
+        self.label = QtWidgets.QLabel("hello")
+        vertical_layout.addWidget(self.label)
+
+        self.statusBar().showMessage("Ready")
+        self.show()
+
+    def open_file(self):
+        file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open Image File", QtCore.QDir.homePath())
+        if file_name:
+            self.pixmap = QtGui.QPixmap(file_name)
+            self.label.setPixmap(self.pixmap)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+    window = MainWindow()
+    exit(app.exec())
